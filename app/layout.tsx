0da950e959e577b4ba45fe73607393b1b7deaad5
Joni Lassila <joni@lassila.live>
2025-02-22 18:49:33 +0200

    Add adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4867502528496793"
+     crossOrigin="anonymous"></script>
       <meta name="google-adsense-account" content="ca-pub-4867502528496793"></meta>
       </head>
       <body
